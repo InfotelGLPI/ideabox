fceee7f9c36ad6ff25c7f54104886b28dcb06758
Thetsmr <thetsmr@gmail.com>
2026-05-29 12:52:36 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_IDEABOX_VERSION', '4.0.8');
+define('PLUGIN_IDEABOX_VERSION', '4.0.9');
 
 global $CFG_GLPI;
 
