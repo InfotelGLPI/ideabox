fdae26a72297a0210f036f853ecb23e874fa3857
Thetsmr <thetsmr@gmail.com>
2026-08-22 12:06:16 +0200

    Fix headers

--- tools/regenerate_headers.sh
+++ tools/regenerate_headers.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
-# -------------------------------------------------------------------------
-# Regenerate GPL headers for all PHP files of the accounts plugin.
+
 #
-# Usage (from any directory):
-#   bash tools/regenerate_headers.sh [--dry-run]
-#
-# --dry-run  Show what would change without writing anything.
 # -------------------------------------------------------------------------
+# ideabox plugin for GLPI
+# Copyright (C) 2025-2026 by the ideabox Development Team.
+#
+# https://github.com/InfotelGLPI/ideabox
+# -------------------------------------------------------------------------
+#
+# LICENSE
+#
+# This file is part of ideabox.
+#
+# ideabox is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# ideabox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ideabox. If not, see <http://www.gnu.org/licenses/>.
+# --------------------------------------------------------------------------
+#
+
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
